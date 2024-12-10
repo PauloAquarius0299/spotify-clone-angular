fe2ae490e77dd9d9fc6834a7b4ba8e45962efbb8
Paulo Cesar <paulocgsantos99@gmail.com>
2024-12-10 17:47:10 -0300

    initial project deploy

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const enviroment = {
+  production: false
+}
+
+export const SpotifyConfiguration = {
+  clientId: '6a65869fea514793b141de0e1f15c3f9',
+  authEndpoint: 'http://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
+}
